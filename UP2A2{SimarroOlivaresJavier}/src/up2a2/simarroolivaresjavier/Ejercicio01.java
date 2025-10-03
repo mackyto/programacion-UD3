6df84edd4b16cc392f1d9499859d722f1a4680aa
SIMARRO OLIVARES - JAVIER <29160712r@CV3445.local>
2025-10-03 20:31:19 +0200

    ejercicio 1 sin acabar

---
+++ UP2A2{SimarroOlivaresJavier}/src/up2a2/simarroolivaresjavier/Ejercicio01.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package up2a2.simarroolivaresjavier;
+import java.util.Scanner
+
+/**
+ *Crea un programa que calcule el IVA (21%) de un precio introducido por teclado y muestre tanto la base imponible como el total.
+ * 
+ * 
+ * @author 29160712r
+ */
+public class Ejercicio01 {
+
+    public static void main(String[] args){
+        Sting datos;
+        double cantidad;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Indica el precio para calcular el IVA (21%)");
+        System.err.println("Porfabor introduce solo números o dará error,");
+        String datos=entrada.nexline();
+        
+        
+    }
+
+}
---
+++ UP2A2{SimarroOlivaresJavier}/src/up2a2/simarroolivaresjavier/UP2A2SimarroOlivaresJavier.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package up2a2.simarroolivaresjavier;
+
+/**
+ *
+ * @author 29160712r
+ */
+public class UP2A2SimarroOlivaresJavier {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
