ac03df6377c5d4a889572bef1bf17f9ffe6b6a9a
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-10-04 18:24:52 +0200

    Ejercicios 1,2 y 3 acabadosincluidos

---
+++ UP2A2{SimarroOlivaresJavier}/src/up2a2/simarroolivaresjavier/Ejercicio02.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package up2a2.simarroolivaresjavier;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/**
+ * 2. Convierte una temperatura en grados Celsius a Kelvin, utilizando la fórmula apropiada,
+ * partiendo de la temperatura dada por el usuario.
+ * 
+ * @author 29160712r
+ */
+
+public class Ejercicio02 {
+
+    public static void main(String[] args){
+        
+        System.out.println();
+        System.err.println();
+        System.out.println("Indica una temperatura en grados celsius para calcular su valor en fahrenheit");
+        System.err.println("Porfabor introduce solo números con comas para decimales o dará error,");
+        Scanner keyld = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.0");
+        Double celsius =  keyld.nextDouble();
+        System.out.println(celsius+"º Celsius son "+df.format((celsius*9/5)+32)+"º Fahrenheit");
+        
+    }
+  
+}
---
+++ UP2A2{SimarroOlivaresJavier}/src/up2a2/simarroolivaresjavier/Ejercicio03.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package up2a2.simarroolivaresjavier;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+        
+/**
+ *3. Desarrolla un programa que solicite el importe de una cuenta en un restaurante
+ *   y calcule tres opciones de propina: 10%, 15% y 20%.
+ * 
+ * @author 29160712r
+ */
+
+public class Ejercicio03 {
+    
+    public static void main(String[] args){
+        
+        Scanner keyld = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.print("La suma de la cuenta del restaurante es de ");
+        Double suma = keyld.nextDouble();
+        System.out.println("Al 10% de propina debes dar "+df.format(suma*0.1)+"€");
+        System.out.println("Al 15% de propina, esta sube a "+df.format(suma*0.15)+"€");
+        System.out.println("Al 20% de propina tiene un coste de "+df.format(suma*0.2)+"€");
+        
+    }
+    
+}
