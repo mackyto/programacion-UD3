1adb2fa69f882bd9557153a5efe242ba808d22fa
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-10-29 16:53:35 +0100

    prue

--- UD3A3{SimarroOlivaresJavier}/src/Ejercicio03.java
+++ UD3A3{SimarroOlivaresJavier}/src/Ejercicio03.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 
 /**
  *
+ * Crea un programa que defina una contraseña en una constante y le pida al
+ * usuario que la introduzca. Si es correcta, mostrará un mensaje indicándolo.
+ * Si no, tras tres intentos fallidos mostrará un mensaje de error.
+ * 
  * @author 29160712r
  */
 public class Ejercicio03 {
@@ -17,13 +21,28 @@ public class Ejercicio03 {
     public static void main(String[] args){
         
         Scanner kl = new Scanner(System.in);
+        final String CONTRASEÑA = "hqxjt8Z%";
+        String texto = "";
         
-        System.out.print("dhdhdhdhjhdjhdjhdjdhdjhdjdh");
-        
-        int numero = kl.nextInt();
+        for (int i = 1; i <= 3; i++){
+            
+            switch (i){
+                    
+                case 1:System.out.print("Por favor, introduzca la contraseña ");break;
+                case 2:System.out.print("Segundo intento, por favor introduzca la contraseña ");break;
+                case 3:System.out.printf("%sÚltimo intento, por favor introduzca, por ultima vez, la contraseña %s", ROJO, RESET);break;
+            
+            }
+                
+            if (CONTRASEÑA.equals(kl.nextLine())){
+            
+                System.out.println("Contraseña correcta");
+                break;
+                
+            }else if (i == 3){System.err.println("¡¡¡MAL111 has Fallado todos los intentos");}            
+            
+        }
 
-        int caca = kl.nextInt();
-        
     }
     
 }
