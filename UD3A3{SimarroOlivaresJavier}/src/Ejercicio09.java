7b9d19e446d2d4f805bceceb047c67e2b34e0008
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-11-02 13:55:56 +0100

    UD3 new

---
+++ UD3A3{SimarroOlivaresJavier}/src/Ejercicio09.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * 9. Crea un programa que pida al usuario que indique cuántas notas va a
+ *    introducir y, a continuación, lea ese número de notas entre 0 y 10.
+ *    Tras ello, debe imprimir por pantalla la nota media.
+ * 
+ * @author 29160712r
+ */
+public class Ejercicio09 {
+    
+    public static final String RESET = "";
+    public static final String ROJO = "\u001B[31m";
+    
+    public static void main(String[] args){
+        
+        Scanner kl = new Scanner(System.in);
+        
+        System.out.print("Indique cuantas notas va a introducir: ");
+        int cantidad = 0;
+        double nota = 0.00;
+        
+        System.out.println("Introduzca las notas de 0 a 10, con o sin decimales. Se calcularan un máximo de tres decimales");
+
+        for(int i = cantidad; i>=1; i++){
+            
+            System.out.printf("Introduce la nota (quedan %d) ", i);
+            nota += kl.nextDouble();
+            
+        }
+        
+        System.out.println("__________");
+        System.out.println("|        |");
+        System.out.println("|  Nota  |");
+        System.out.println("| Media  |");
+        System.out.println("|--------|");
+        System.out.println("|        |");
+        System.out.println("|  %.3f  |");
+        System.out.println("|--------|");
+  
+        
+        
+    }
+    
+}
