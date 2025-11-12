67d5b304112e10210688fca748d7e0b132015113
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-11-12 18:09:43 +0100

    prue

---
+++ UD4A2{SimarroOlivaresJavier}/src/BucleIteraciones.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author javsimoli
+ */
+public class BucleIteraciones {
+    public static void main(String[] args){
+        
+        int debug_test = 1;
+        Scanner sc = new Scanner(System.in);
+        debug_test = 2;
+        
+        System.out.print("¿Cuantas iteraciones quieres hacer?");
+        debug_test = 3;
+        
+        int numIteraciones = sc.nextInt();
+        debug_test = 4;
+        
+        // Bucle 10 iteraciones o las que indique el usuario
+        for(int i=1; i <= numIteraciones; i++){
+            
+            System.out.println("");
+        }
+        
+        
+    }
+    
+    
+}
