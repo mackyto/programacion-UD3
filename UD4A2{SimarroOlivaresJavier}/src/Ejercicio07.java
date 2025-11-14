18e0cfbaaab10bc3479db043e1f90d25d57a9585
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-11-14 17:55:29 +0100

    prue

--- UD4A2{SimarroOlivaresJavier}/src/Ejercicio07.java
+++ UD4A2{SimarroOlivaresJavier}/src/Ejercicio07.java
@@ -17,19 +17,87 @@ import java.util.Scanner;
  */
 public class Ejercicio07 {
     
-    public static void main(String[] args){
+    public static final String RESET = "\u001B[0m";                             // Define colores ANSI en constantes.
+    public static final String ROJO = "\u001B[31m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String FONDO_GRIS = "\u001B[47m";
+    
+    public static void main(String[] args){                                     // Declara el método main.
         
-        Scanner kl = new Scanner(System.in);
-        int[][] numeros = new int[20][20];
-        int fila = 0, columna = 0, data = 0;
+        Scanner kl = new Scanner(System.in);                                    // Inincializa la instancia Scanner.
         
-        do{
-            
-            System.out.print("");
-            
-            
-        }while(data != -1);
+        int[][] numeros = new int[20][20];                                      // Declara las matrices.
+        boolean[][] cambios = new boolean[20][20];        
         
+        int linea = 0, columna = 0, data = 0;                                    // Declara e inicia las variables.
+        
+        for (int i = 0; i < numeros.length; i++){                               // carga la matriz numeros con una sucesión incremental de enteros.
+            for (int j = 0; j < numeros[i].length; j++){
+                numeros[i][j] = i + j;cambios[i][j] = false;
+        }}     
+
+
+        do{                                                                     // Bucle principal de actualización de datos.
+            
+            System.out.println("Para salir introduce '-1' en cualquier entrada del datos");
+            System.out.println();
+ 
+            System.out.print(AZUL + "Indica la Línea o Array de 1-20 ");        // Indica la fila, sale con -1
+            linea = kl.nextInt();
+            if(linea == -1){break;}
+            
+            System.out.print(VERDE + "Indica la Columna o Registro de 1-20 ");  // Indica la columna, sale con -1 
+            columna = kl.nextInt();
+            if(columna == -1){break;}
+            
+            do{                                                                 // Introduce el data desde -1 hasta 999 de lo contrario repite 
+            System.out.print(RESET + "Indica el valor del Registro <1000 ");
+            data = kl.nextInt();
+            if(data == -1){break;}
+            }while (data < 0 || data > 999);
+            
+            numeros[--linea][--columna] = data;                                 // Actualiza la matriz de datos y la booleana de gestión de color
+            cambios[linea][columna] = true;
+        
+        }while(true);                                                           // cierra el Bucle sin condición
+        
+        System.out.printf("\n\nmatriz");
+        
+        for (int i =0; i < numeros.length; i++){                                // Bucle exterior impresión de Matriz (filas).
+        
+            System.out.printf("\t{");
+            
+            for (int j = 0; j < numeros[i].length; j++){                         // Bucle anidado interior (columnas). 
+
+                if (numeros[i][j] >= 0){                                         // Ajuste de tabulación dependiendo de la resolución del número.
+                    System.out.print(" ");}                
+                if (numeros[i][j] < 10 && numeros[i][j] > -10){
+                    System.out.print(" ");}
+                if (numeros[i][j] < 100 && numeros[i][j] > -100){
+                    System.out.print(" ");}  
+//                if (numeros[i][j] < 1000 && numeros[i][j] > -1000){
+//                    System.out.print(" ");}
+//                if (numeros[i][j] < 10000 && numeros[i][j] > -10000){
+//                    System.out.print(" ");}
+//                if (numeros[i][j] < 100000 && numeros[i][j] > -100000){
+//                    System.out.print(" ");}
+//                if (numeros[i][j] < 1000000 && numeros[i][j] > -1000000){
+//                    System.out.print(" ");}
+//                if (numeros[i][j] < 10000000 && numeros[i][j] > -10000000){
+//                    System.out.print(" ");}
+                
+                if(cambios[i][j]){                                              // Controla el color de las modificadas
+                    System.out.print(ROJO);
+                }else{System.out.print(RESET);}
+                
+                System.out.printf("%d, ", numeros[i][j]);                        // Impresión de los valores seleccionados de la Matriz. 
+                
+            }
+        
+            System.out.printf("\b\b}\n\n");
+            
+        }
         
     }
     
