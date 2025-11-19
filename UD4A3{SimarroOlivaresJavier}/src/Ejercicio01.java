c4e7671dd167a9b48af59786272b692bf1c75240
Javier Simarro Olivares <javsimoli@alu.edu.gva.es>
2025-11-19 13:50:27 +0100

    UD3 new

--- UD4A3{SimarroOlivaresJavier}/src/Ejercicio01.java
+++ UD4A3{SimarroOlivaresJavier}/src/Ejercicio01.java
@@ -3,10 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
- *
+ * 
+ * 1. Escribe un programa que pida 10 números reales por teclado, los guarde en
+ *    una lista, e imprima todos los valores almacenados.
+ * 
  * @author javsimoli
  */
 public class Ejercicio01 {
+    
+    public static void main(String[] args){
+        
+        Scanner kl = new Scanner(System.in);
+        
+        ArrayList<Integer> numeros =  new ArrayList<>();
+        
+        for (int i = 0; i < numeros.size(); i++){
+            
+            System.out.print("Introduce el numero " + i);
+            numeros.add(1) = kl.nextInt();
+            
+            
+        }
+        
+    }
+    
+    
     
 }
